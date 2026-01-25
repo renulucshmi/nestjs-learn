1db7f771eef18ea4179def63abdfa05f4970b37f
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2026-01-25 20:21:26 +0530

    feat: Initialize NestJS application with a basic controller and an empty Tweet module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { userModule } from './users/users.module';
+import { TweetModule } from './tweet/tweet.module';
 
 @Module({
-  imports: [userModule],
+  imports: [userModule, TweetModule],
   controllers: [AppController],
   providers: [AppService],
 })
